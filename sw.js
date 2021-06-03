7d84b84fb46ee7fe2346e1f177412ce770414a55
zxm1220 <18352539920@163.com>
2021-06-03 11:32:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
 if (workbox) {
     workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' });
 
-    workbox.precaching.precache(['/', '/index.html']);
+    workbox.precaching.precache(['/blog/', '/index.html']);
 
     workbox.routing.registerRoute(new RegExp('^https?://x1nes.github.io/blog/?$'), workbox.strategies.networkFirst());
 
